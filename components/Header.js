d3609128a613b7f30f4891f1fede3d3294c9c398
timaboon <tymofiy@zelexio.com>
2022-01-31 23:32:05 -0500

    made header work with user, not finished but good for now

--- components/Header.js
+++ components/Header.js
@@ -1,45 +1,146 @@
-import { HomeIcon } from "@heroicons/react/outline";
+import { GlobeAltIcon, HomeIcon } from "@heroicons/react/outline";
 import { useRouter } from "next/router";
+import AuthService from "../services/AuthService";
+import { UserCircleIcon } from "@heroicons/react/solid";
+import Image from "next/image";
 
-function Header() {
+function Header(props) {
+  let headerSpan = 2;
   const router = useRouter();
+  let user = props?.user;
+  const logoutHandler = async () => {
+    try {
+      await AuthService.logout();
+      router.reload();
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
+  const userImage = () => {
+    if (user?.image === null) {
+      return (
+      <div className="dropdown relative h-12 w-12 mr-5 rounded-2xl dropdown-end">
+          <UserCircleIcon
+            className="h-12 btn-ghost text-gray-300 cursor-pointer hover:scale-105"
+            tabindex="0"
+          />
+          
+          <ul
+            tabindex="0"
+            class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Account Information</a>
+            </li>
+
+            <li>
+              <a onClick={logoutHandler}>Sign Out</a>
+            </li>
+          </ul>
+        </div>)
+    } else {
+      return (
+        <div className="dropdown relative h-12 w-12 mr-5 rounded-2xl dropdown-end">
+          <Image
+            src={user?.image}
+            layout="fill"
+            alt="user"
+            className="rounded-full btn mb-1 hover:scale-105"
+            tabindex="0"
+          />
+          ;
+          <ul
+            tabindex="0"
+            class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Account Information</a>
+            </li>
+
+            <li>
+              <a onClick={logoutHandler}>Sign Out</a>
+            </li>
+          </ul>
+        </div>
+      );
+    }
+  };
+  if (user !== undefined && user !== null) {
+    let roles = [];
+    document.getElementById("hd").classList.remove("grid-cols-3");
+    document.getElementById("hd").classList.add("grid-cols-2");
+    for (let i = 0; i < user.roles.length; i++) {
+      roles.push(user.roles[i].name);
+    }
+    if (roles.indexOf("ROLE_HOST") > -1) {
+      console.log("woohoo you are a host");
+      // return (
+      //   <div id="" className="items-stretch lg:flex justify-between hidden">
+      //     <a className="btn btn-ghost rounded-btn hover:bg-blue-800 w-1/3">
+      //       Calendar
+      //     </a>
+      //     <a className="btn btn-ghost rounded-btn hover:bg-blue-900 w-1/3">
+      //       Properties
+      //     </a>
+      //     <a className="btn btn-ghost rounded-btn hover:bg-blue-900 w-1/3">
+      //       Notifications
+      //     </a>
+      //   </div>
+      // );
+    }
+    if (roles.indexOf("ROLE_GUEST") > -1) {
+      console.log("woohoo you are a guest");
+    }
+  }
   return (
-    <header className="top-0 z-50 grid grid-cols-2 shadow-md p-3 md:px-3
-    navbar mb-2 text-neutral-content rounded-box">
+    <header
+      id="hd"
+      className="top-0 z-50 grid grid-cols-2 shadow-md p-3 md:px-3 navbar mb-2 text-neutral-content rounded-box"
+    >
       {/* Left */}
       <div
-        onClick={() => router.push("/")}
-        className="relative items-center h-10 cursor-pointer content-center my-auto text-gray-100
-        flex-none px-2 mx-2"
+        className="relative items-center h-10  content-center my-auto text-gray-100
+        flex-none px-2 mx-2 "
       >
-        <HomeIcon className="h-9 flex-none justify-left"/>
-        <p className="text-3xl font-bold select-none justify-left ml-2 mr-4">Hatarent</p>
-      
-    <div className="items-stretch lg:flex justify-between">
-      <a className="btn btn-ghost rounded-btn hover:bg-blue-800 w-1/3">
-              Calendar
-            </a> 
-      <a className="btn btn-ghost rounded-btn hover:bg-blue-900 w-1/3">
-              Properties
-            </a> 
-      <a className="btn btn-ghost rounded-btn hover:bg-blue-900 w-1/3">
-              Notifications
-            </a>
-    </div>
-
-  </div>
-
-  <div className="flex-none justify-end">
-    <button className="btn transition-colors duration-150 border border-blue-500 rounded-lg focus:shadow-outline hover:bg-blue-00 bg-blue-500 hover:text-blue-500 text-blue-100">
-      <a>SIGN OUT</a>
-    </button>
-    <div className="avatar">
-      <div className="rounded-full w-10 h-10 m-1 mx-4">
-        <img src="https://media.discordapp.net/attachments/783168555591794708/930486752316129371/Yohho.jpg?width=885&height=590"></img>
+        <div
+          onClick={() => router.push("/")}
+          className="cursor-pointer flex hover:scale-105"
+        >
+          <HomeIcon className="h-9 flex-none justify-start  " />
+          <p className="text-3xl font-bold select-none justify-left ml-2 mr-4">
+            Hatarent
+          </p>
+        </div>
       </div>
-    </div>
-  </div>
+      {/* Middle */}
+
+      {/* Right */}
+      <div className="flex items-center space-x-4 justify-end text-gray-800">
+        {!user && (
+          <div className="relative flex  items-center align-middle">
+            <div
+              onClick={() => {
+                router.push({
+                  pathname: "/register",
+                  query: { role: "host" },
+                });
+              }}
+              className="flex mr-5 cursor-pointer hover:scale-105"
+            >
+              <p className="hidden md:inline  text-white">Become a host</p>
+              <GlobeAltIcon className="h-6  text-white" />
+            </div>
+            <button
+              className="btn btn-ghost text-white hover:scale-105 active:bg-blue-800"
+              onClick={() => router.push("/login")}
+            >
+              Sign In
+            </button>
+          </div>
+        )}
+        {user && userImage()}
+      </div>
     </header>
   );
 }
