7f5958a7c53ad247c0c99251d0888e5e142c9ae1
timaboon <tymofiy@zelexio.com>
2022-01-31 23:35:02 -0500

    Added addressHandler to parse address data

--- helper/AddressHandler.js
+++ helper/AddressHandler.js
@@ -1,6 +1,6 @@
 class AddressHandler {
   getAddress(addressRowData) {
-    if ("address" in addressRowData) {
+    if (addressRowData.hasOwnProperty("address")) {
       let place_name = addressRowData.place_name;
       const pattern = /(.*), (.*), (\S+) (.*), (.*)/;
       const address = place_name.split(pattern);
@@ -31,6 +31,64 @@ class AddressHandler {
       ", " +
       addressObject.country;
     return addressString;
+  }
+  getSearchAddress(addressRowData) {
+    if (addressRowData.hasOwnProperty("address")) {
+      let place_name = addressRowData.place_name;
+      const pattern = /(.*), (.*), (\S+) (.*), (.*)/;
+      const address = place_name.split(pattern);
+      const addressObject = {
+        city: address[2],
+        province: address[3],
+        country: address[5],
+      };
+      return addressObject;
+    } else {
+      let place_name = addressRowData.place_name;
+      const pattern = /(.*), (.*), (.*)/;
+      const address = place_name.split(pattern);
+      const addressObject = {
+        city: address[1],
+        province: address[2],
+        country: address[3],
+      };
+      return addressObject;
+    }
+  }
+  getSearchAddressQuery(addressRowData) {
+    const pattern = /(.*), (.*), (.*)/;
+    const address = addressRowData.split(pattern);
+    const addressObject = {
+      city: address[1],
+      province: address[2],
+      country: address[3],
+    };
+    return addressObject;
+  }
+  getSearchAddressString(addressObject) {
+    if (addressObject.hasOwnProperty("street")) {
+      let addressString = "";
+      addressString =
+        addressObject.street +
+        ", " +
+        addressObject.city +
+        ", " +
+        addressObject.province +
+        ", " +
+        addressObject.zip +
+        ", " +
+        addressObject.country;
+      return addressString;
+    } else {
+      let addressString = "";
+      addressString =
+        addressObject.city +
+        ", " +
+        addressObject.province +
+        ", " +
+        addressObject.country;
+      return addressString;
+    }
   }
 }
 export default new AddressHandler();
