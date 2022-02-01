16f0526b8b444938480cb3f6db29594e4bdc86fc
timaboon <tymofiy@zelexio.com>
2022-01-31 23:31:22 -0500

    made update property design change

--- pages/update-property.js
+++ pages/update-property.js
@@ -3,53 +3,79 @@ import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import PropertyForm from "../components/PropertyForm";
+import AuthService from "../services/AuthService";
 import PropertyService from "../services/PropertyService";
 
 function UpdateProperty() {
   const router = new useRouter();
-  const [isMounted, setIsMounted] = useState(false);
-  const [uuid, setUuid] = useState(null);
   const [data, setData] = useState(null);
+  const [pathname, setPathname] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [isMounted, setIsMounted] = useState(false);
+
 
   async function getData() {
     try {
+      setLoading(true);
+      setData(null);
+
       let res = await PropertyService.getByUuidProperty(router.query.uuid);
       let dataResponse = await res.data;
       setData(dataResponse);
     } catch (e) {
       console.log(e);
+    } finally {
+      setLoading(false);
     }
   }
-  useEffect(() => {
-    if (router.isReady) {
-      console.log(router.query.uuid);
-      setUuid(router.query.uuid);
-    }
+  
+  useEffect(async () => {
     setIsMounted(true);
   }, []);
+  useEffect(async () => {
+    if (isMounted == true) {
+      try {
+        let userFound = await AuthService.getUser();
+        setUser(userFound.data);
+      } catch (e) {}
+    }
+  }, [isMounted]);
+
 
   useEffect(() => {
     if (!router?.isReady) return;
     getData();
+    setPathname(router.pathname);
   }, [router.isReady]);
+  let loader = null;
 
-  if (!isMounted)
+  if (loading) {
+    loader = (
+      <div className="h-screen ">
+        <div className="flex justify-center items-center h-screen">
+          <div className="flex items-center justify-center space-x-2 animate-bounce">
+            <div className="w-20 h-20 bg-blue-400 rounded-full"></div>
+            <div className="w-20 h-20 bg-green-400 rounded-full"></div>
+            <div className="w-20 h-20 bg-black rounded-full"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  let content = null;
+
+  if (data != null) {
+    content = <PropertyForm data={data} pathname={pathname} />;
+  }
   return (
     <>
-      <div className="flex items-center justify-center space-x-2 animate-bounce">
-        <div className="w-8 h-8 bg-blue-400 rounded-full"></div>
-        <div className="w-8 h-8 bg-green-400 rounded-full"></div>
-        <div className="w-8 h-8 bg-black rounded-full"></div>
-      </div>
-    </>
-  );
-  return (
-    <div>
-      <Header />
-      {data && (<PropertyForm data={data}/>)}
-      
+      <Header user={user}/>
+      {loader}
+      {content}
       <Footer />
-    </div>
+    </>
   );
 }
 
