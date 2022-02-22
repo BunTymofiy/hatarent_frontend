29b2ccabbb0c57e876b42729e5537647dee333b0
timaboon <tymofiy@zelexio.com>
2022-02-22 09:19:39 -0500

    added a lot of stuff

--- services/AuthService.js
+++ services/AuthService.js
@@ -26,7 +26,7 @@ class AuthService
     }
     logout()
     {
-        return axios.head('https://hatarent-frontend.vercel.app./api/logout')
+        return axios.head('https://hatarent-frontend.vercel.app/api/logout')
     }
     getUserByUuid(uuid)
     {
